4b1806d5349d794465f73f18ba49c67bcb094cf9
김경진 <kkj9406@gmail.com>
2013-04-03 13:39:58 +0900

    인터셉터 부분 추가 주석

--- src/main/java/egovframework/rte/cmmn/interceptor/AuthenticInterceptor.java
+++ src/main/java/egovframework/rte/cmmn/interceptor/AuthenticInterceptor.java
@@ -24,11 +24,13 @@ public class AuthenticInterceptor extends HandlerInterceptorAdapter {
 		// HandlerInterceptorAdapter는 preHandle, postHandle, afterCompletion을 지원하고
 		// 계정이 세션에 있는지 검사하여 리다이렉트 시키는 작업은 요청 전에 해야하므로
 		// preHandle 클래스를 오버라이드하여 사용한다.
-		Account account = (Account) WebUtils.getSessionAttribute(request, "UserAccount");
 		
-		if(account!=null){
+		Account account = (Account) WebUtils.getSessionAttribute(request, "UserAccount");
+		//account형의 세션을 받아서
+		
+		if(account!=null){//세션에 계정이 있으면 true를 반납하고 
 			return true;
-		}else{
+		}else{//계정이 없다면 로그인화면으로 리다이렉트 한다.
 			ModelAndView modelAndView = new ModelAndView("redirect:/sample/login.do");			
 			throw new ModelAndViewDefiningException(modelAndView);
 		}
