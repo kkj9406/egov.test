42f0d845232806e08c9bd1d843efc69f3eb6f0ec
김경진 <kgw9406@naver.com>
2013-04-05 12:54:54 +0900

    AuthenticInterceptor.java 주석 추가
    ModelAndView 설�

--- src/main/java/egovframework/rte/cmmn/interceptor/AuthenticInterceptor.java
+++ src/main/java/egovframework/rte/cmmn/interceptor/AuthenticInterceptor.java
@@ -31,7 +31,19 @@ public class AuthenticInterceptor extends HandlerInterceptorAdapter {
 		if(account!=null){//세션에 계정이 있으면 true를 반납하고 
 			return true;
 		}else{//계정이 없다면 로그인화면으로 리다이렉트 한다.
-			ModelAndView modelAndView = new ModelAndView("redirect:/sample/login.do");			
+			ModelAndView modelAndView = new ModelAndView("redirect:/sample/login.do");
+			/* ModelAndView 클래스는 컨트롤러의 처리 결과를 보여줄 view와
+			 * view에서 사용할 값을 전달하는 클래스이다. 여기서는 String형의
+			 * viewName을 매개변수로 전달하므로 dispatcher-servlet에 등록된
+			 * viewResolver에 의해 view 객체를 찾아간다. 
+			 * ModelAndView로 포워드 뿐만 아니라 리다이렉트도 사용할 수 있는데
+			 * 이런 경우에는 setViewName() 메소드 파라미터에 "redirect:"를 
+			 * prefix로 붙여주면 된다. /main.jsp로 보내고 싶다면 아래와 같이 사용하면 된다
+			 * return ModelAndView("redirect:/main.jsp");
+			 * 혹시나 다른 URL로 포워드 하고 싶으면 "forward:"를 prefix로 붙이면 된다
+			 * forward와 redirect 차이는 클라이언트와 통신하느냐, 
+			 * request가 유지되느냐, 최종 URL이 변경되느냐 정도이다
+			 * */
 			throw new ModelAndViewDefiningException(modelAndView);
 		}
 	}
